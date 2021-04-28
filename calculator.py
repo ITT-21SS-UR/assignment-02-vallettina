0fd6f5763c3f1acdfb1514712c4240164c359973
vallettina <elisa.v@gmx.de>
2021-04-28 14:04:08 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,3 +1,7 @@
+# Implement a simple graphical calculator using PyQt.
+# Source for the basic structure of the calculator: https://www.geeksforgeeks.org/building-calculator-using-pyqt5-in-python/
+
+
 import sys
 from PyQt5 import uic, Qt
 from PyQt5.QtWidgets import QMainWindow
@@ -7,130 +11,65 @@ class Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi("calculator.ui", self)
-        self.initButtons()
+        self.init_buttons()
 
-    def initButtons(self):
+    def init_buttons(self):
         # clear text
         self.ui.lcd_result.setText("")
         # number buttons
-        self.ui.btn_0.clicked.connect(self.action_0)
-        self.ui.btn_1.clicked.connect(self.action_1)
-        self.ui.btn_2.clicked.connect(self.action_2)
-        self.ui.btn_3.clicked.connect(self.action_3)
-        self.ui.btn_4.clicked.connect(self.action_4)
-        self.ui.btn_5.clicked.connect(self.action_5)
-        self.ui.btn_6.clicked.connect(self.action_6)
-        self.ui.btn_7.clicked.connect(self.action_7)
-        self.ui.btn_8.clicked.connect(self.action_8)
-        self.ui.btn_9.clicked.connect(self.action_9)
+        self.ui.btn_0.clicked.connect(lambda x: self.action('0'))
+        self.ui.btn_1.clicked.connect(lambda x: self.action('1'))
+        self.ui.btn_2.clicked.connect(lambda x: self.action('2'))
+        self.ui.btn_3.clicked.connect(lambda x: self.action('3'))
+        self.ui.btn_4.clicked.connect(lambda x: self.action('4'))
+        self.ui.btn_5.clicked.connect(lambda x: self.action('5'))
+        self.ui.btn_6.clicked.connect(lambda x: self.action('6'))
+        self.ui.btn_7.clicked.connect(lambda x: self.action('7'))
+        self.ui.btn_8.clicked.connect(lambda x: self.action('8'))
+        self.ui.btn_9.clicked.connect(lambda x: self.action('9'))
         # arithmetic signs
-        self.ui.btn_equals.clicked.connect(self.action_equals)
-        self.ui.btn_plus.clicked.connect(self.action_plus)
-        self.ui.btn_minus.clicked.connect(self.action_minus)
-        self.ui.btn_div.clicked.connect(self.action_div)
-        self.ui.btn_multi.clicked.connect(self.action_multi)
+        self.ui.btn_equals.clicked.connect(lambda x: self.action('='))
+        self.ui.btn_plus.clicked.connect(lambda x: self.action('+'))
+        self.ui.btn_minus.clicked.connect(lambda x: self.action('-'))
+        self.ui.btn_div.clicked.connect(lambda x: self.action('/'))
+        self.ui.btn_multi.clicked.connect(lambda x: self.action('*'))
         # other buttons
-        self.ui.btn_clean.clicked.connect(self.action_clean)
-        self.ui.btn_delete.clicked.connect(self.action_delete)
-        self.ui.btn_point.clicked.connect(self.action_point)
+        self.ui.btn_clean.clicked.connect(lambda x: self.action('CLEAN'))
+        self.ui.btn_delete.clicked.connect(lambda x: self.action('DELETE'))
+        self.ui.btn_point.clicked.connect(lambda x: self.action('.'))
 
-    # number methods
-    def action_0(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "0")
-        print(equation + "0")
 
-    def action_1(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "1")
-        print(equation + "1")
+    def log_button(message):
+        def func_decorator(func):
+            def logging(self, sign):
+                print(message + sign)
+                func(self, sign)
+            return logging
+        return func_decorator
 
-    def action_2(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "2")
-        print(equation + "2")
-
-    def action_3(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "3")
-        print(equation + "3")
-
-    def action_4(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "4")
-        print(equation + "4")
-
-    def action_5(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "5")
-        print(equation + "5")
-
-    def action_6(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "6")
-        print(equation + "6")
-
-    def action_7(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "7")
-        print(equation + "7")
-
-    def action_8(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "8")
-        print(equation + "8")
-
-    def action_9(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "9")
-        print(equation + "9")
-
-    # arithmetic signs
-    def action_plus(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "+")
-        print(equation + "+")
-
-    def action_minus(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "-")
-        print(equation + "-")
-
-    def action_multi(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "*")
-        print(equation + "*")
-
-    def action_div(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + "/")
-        print(equation + "/")
-
-    def action_equals(self):
+    @log_button("Button clicked: ")
+    def action(self, sign):
         equation = self.ui.lcd_result.text()
 
-        try:
-            result = eval(equation)
-            self.ui.lcd_result.setText(str(result))
-            print(result)
-        except:
-            self.ui.lcd_result.setText("ERROR")
-            print("ERROR")
+        if sign in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            self.ui.lcd_result.setText(equation + sign)
 
-    # other buttons
-    def action_point(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation + ".")
-        print(equation + ".")
+        if sign in [".", "+", "-", "*", "/"]:
+            self.ui.lcd_result.setText(equation + sign)
 
-    def action_delete(self):
-        equation = self.ui.lcd_result.text()
-        self.ui.lcd_result.setText(equation[:len(equation) - 1])
-        print(equation[:len(equation) - 1])
+        if sign == "DELETE":
+            self.ui.lcd_result.setText(equation[:len(equation) - 1])
 
-    def action_clean(self):
-        self.ui.lcd_result.setText("")
-        print("")
+        if sign == "CLEAN":
+            self.ui.lcd_result.setText("")
+
+        if sign == "=":
+            try:
+                result = eval(equation)
+                self.ui.lcd_result.setText(str(result))
+            except:
+                self.ui.lcd_result.setText("ERROR")
+                print("ERROR")
 
 
 def main():
